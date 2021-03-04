647e24bed1b00ad13e4a723cbbc9537d74f496a2
Cmarescot <78388833+Cmarescot@users.noreply.github.com>
2021-03-03 20:29:42 -0500

    Adding Py;oll.py file.

--- PyPoll.py
+++ PyPoll.py
@@ -50,9 +50,9 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
-# Print each row in the CSV file.
-    for row in file_reader:
-        print(row)
+# # Print each row in the CSV file.
+#     for row in file_reader:
+#         print(row)
 
         
     # Read and print the header row.
